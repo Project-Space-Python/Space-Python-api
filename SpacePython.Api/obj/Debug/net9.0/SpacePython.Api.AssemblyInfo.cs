1d9e9cbabc5b1dc6eccad34ee9d203314682bc21
Ashton Forsberg <ashtonforsbergg@gmail.com>
2025-03-24 18:23:51 -0400

    Catalog API: get item,new item, new rating by ID, put item, delete item, and Data Access

--- SpacePython.Api/obj/Debug/net9.0/SpacePython.Api.AssemblyInfo.cs
+++ SpacePython.Api/obj/Debug/net9.0/SpacePython.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a868dbc3fefc1951db485744ef652da0a74aca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58479e00e011debe39daa066d7c8731d3fa70ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
