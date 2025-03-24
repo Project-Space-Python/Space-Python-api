c92db9efb5b17287dfdfbdaa47daabf6e108501f
Ashton Forsberg <ashtonforsbergg@gmail.com>
2025-03-24 18:24:13 -0400

    push

--- SpacePython.Api/Controllers/CatalogController.cs
+++ SpacePython.Api/Controllers/CatalogController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using SpacePython.Domain.Catalog;
 
@@ -16,5 +17,35 @@ namespace SpacePython.Api.Controllers
         {
             return Ok("hello world.");
         }
-    }
+        [HttpGet("{id:int}")]
+public IActionResult GetItem(int id)
+{
+    var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+    item.Id = id;
+
+    return Ok(item);
 }
+[HttpPost]
+public IActionResult Post(Item item){
+    return Created("/catalog/42", item);
+}
+[HttpPost("{id:int}/ratings")]
+public IActionResult PostRating(int id, [FromBody] UserRating rating){
+    var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+    item.Id = id;
+    item.AddRating(rating);
+
+    return Ok(item);
+}
+[HttpPut("{id:int}")]
+public IActionResult Put(int id, Item item){
+    return NoContent();
+}
+[HttpDelete("{id:int}")]
+public IActionResult Delete(int id){
+    return NoContent();
+}
+    }
+    
+}
+
