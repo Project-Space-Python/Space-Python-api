309019d7ff72e1da0b0ab10010884bfca18327ff
michaelcogossi <michael.cogossi254@gmail.com>
2025-03-26 20:48:37 -0400

    idk

--- SpacePython.Api/obj/Debug/net9.0/SpacePython.Api.AssemblyInfo.cs
+++ SpacePython.Api/obj/Debug/net9.0/SpacePython.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de059137a462c0c4e16fe1dbb0c9ea6ab7cdd7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85554a08fb5f76324f59f2b8bd3731fa8235290e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
