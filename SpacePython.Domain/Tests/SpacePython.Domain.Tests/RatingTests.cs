ef4f83ac18ac9bc3c71acf6e81532a4d4d89444d
Ashton Forsberg <ashtonforsbergg@gmail.com>
2025-04-04 13:42:37 -0400

    RatingTests

--- SpacePython.Domain/Tests/SpacePython.Domain.Tests/RatingTests.cs
+++ SpacePython.Domain/Tests/SpacePython.Domain.Tests/RatingTests.cs
@@ -17,19 +17,27 @@ internal class TestMethodAttribute : Attribute
 {
 }
 
-internal class RatingTests : Attribute
+internal interface IRatingTests
 {
+    void Cannot_Create_Rating_With_Invalid_Stars();
+    void Can_Create_New_Rating();
 }
-[TestMethod]
-public void Can_Create_New_Rating()
-{
-    var rating = new UserRating(1, "Mike", "Great fit!");
-    Assert.AreEqual(1, rating.Stars);
-    Assert.AreEqual("Mike", rating.UserName);
-    Assert.AreEqual("Great fit!", rating.Review);
 
+internal class RatingTests : Attribute, IRatingTests
+{
 }
 [TestMethod]
-[ExpectedExceptio(typeof(ArgumentException))]{
-    var rating = new RatingTests(0, "Mike", "Great fit!");
-}
+    public void Can_Create_New_Rating()
+    {
+        var rating = new UserRating(1, "Mike", "Great fit!");
+        Assert.AreEqual(1, rating.Stars);
+        Assert.AreEqual("Mike", rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
+
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Cannot_Create_Rating_With_Invalid_Stars()
+    {
+        var rating = new Rating(0, "Mike", "Great fit!");
+    }
